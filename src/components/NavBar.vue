6d5e691964a99dd1f1f269c302141a1f30cb29f7
Bluemethyst <bluemethyst@gmail.com>
2025-01-29 16:55:55 +1300

    init

---
+++ src/components/NavBar.vue
@@ -0,0 +1,48 @@
+<template>
+    <nav class="navbar">
+        <ul class="navbar-list">
+            <li class="navbar-item"><a href="#home">Home</a></li>
+            <li class="navbar-item"><a href="#about">About</a></li>
+            <li class="navbar-item"><a href="#services">Services</a></li>
+            <li class="navbar-item"><a href="#contact">Contact</a></li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'NavBar',
+}
+</script>
+
+<style scoped>
+@import '@/assets/main.css';
+
+.navbar {
+    background-color: rgba(27, 27, 31, 0);
+    overflow: hidden;
+}
+
+.navbar-list {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+}
+
+.navbar-item {
+    padding: 14px 20px;
+}
+
+.navbar-item a {
+    color: white;
+    text-align: center;
+    text-decoration: none;
+    display: block;
+}
+
+.navbar-item a:hover {
+    background-color: #ddd;
+    color: black;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import NavBar from '@/components/NavBar.vue'
+</script>
+
+<template>
+    <main>
+        <NavBar />
+        <div class="main">
+            <div class="card-row">
+                <div class="title-info">
+                    <h1 class="title">Mohua Galactic Trust</h1>
+                    <h3 class="subtitle">Explore The Galaxy Through Your Own Eyes</h3>
+                    <div class="buttons-horizontal">
+                        <RouterLink to="/explore" class="button">Explore</RouterLink>
+                        <RouterLink to="/about" class="button">About</RouterLink>
+                        <RouterLink to="/calendar" class="button">Calendar</RouterLink>
+                        <RouterLink to="/donate" class="button">Donate</RouterLink>
+                    </div>
+                </div>
+            </div>
+            <div class="telescope-div card-row">
+                <div class="gradient-wrapper">
+                    <div class="gradient-circle"></div>
+                    <img class="icon" src="@/assets/telescope.svg" />
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
